45bd46efc8602863d4a39c3509d232de0dc1c2f5
Yogeshcff <yugweeb@gmail.com>
2026-06-16 16:38:17 +0530

    solution

--- Array/next permutation.py
+++ Array/next permutation.py
@@ -1,16 +1,18 @@
-arr=[2,1,5,3,4,0,0]
+arr=[3,2,1]
 index=-1
 n=len(arr)
 for i in range (n-2,-1,-1):
-    if arr[i]<arr[i+1]:
+    if arr[i+1]>arr[i]:
         index=i
+        print(index)
         break
 if index==-1:
-    print(arr[::-1])
+    arr[:]=arr[:][::-1]
+    print(arr)
 else:
     for i in range (n-1,index,-1):
         if arr[i]>arr[index]:
             arr[i],arr[index]=arr[index],arr[i]
             break
     arr[index+1:]=arr[index+1:][::-1]
-print(arr)
+    print(arr)
