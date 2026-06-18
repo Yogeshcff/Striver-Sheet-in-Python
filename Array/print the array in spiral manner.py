757bf328cc19f11886a0704dc42781d08c388eb4
Yogeshcff <yugweeb@gmail.com>
2026-06-18 15:53:54 +0530

    solution

---
+++ Array/print the array in spiral manner.py
@@ -0,0 +1,24 @@
+matrix=[[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+print(matrix)
+print(len(matrix))
+left,top=0,0
+bottom , right=len(matrix)-1,len(matrix[0])-1
+if len(matrix)==0:
+    print([])
+result=[]
+while top<=bottom and left<=right:
+    for i in range (left , right +1):
+        result.append(matrix[top][i])
+    top+=1
+    for i in range (top , bottom+1):
+        result.append(matrix[i][right])
+    right-=1
+    if top<=bottom:
+        for i in range (right,left-1,-1):
+            result.append(matrix[bottom][i])
+        bottom-=1
+    if left<=right:
+        for i in range (bottom,top-1,-1):
+            result.append(matrix[i][left])
+        left+=1
+print(result)        
