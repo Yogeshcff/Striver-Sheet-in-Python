8478d01327bc29b4102a66c5f270817b02665e36
Yogeshcff <yugweeb@gmail.com>
2026-06-20 16:09:45 +0530

    solution

--- Array/hard/maximum product subarray.py
+++ Array/hard/maximum product subarray.py
@@ -1,26 +1,29 @@
-arr=[1,2,-3,0,-4,-5]
-ans=float('-inf')
-n=len(arr)
-pre=1
-suf=1
-for i in range(n):
+# arr=[1,2,-3,0,-4,-5]
+# ans=float('-inf')
+# n=len(arr)
+# pre=1
+# suf=1
+# for i in range(n):
     
-    if pre==0:
-        pre=1
-    if suf==0:
-        suf=1
+#     if pre==0:
+#         pre=1
+#     if suf==0:
+#         suf=1
             
-    pre=pre*arr[i]
-    suf=suf*arr[n-i-1]
-    ans=max(pre,ans,suf)
-print(ans)
+#     pre=pre*arr[i]
+#     suf=suf*arr[n-i-1]
+#     ans=max(pre,ans,suf)
+# print(ans)
 
-arr=[1,2,-3,0,-4,-5]
-ans=float('-inf')
-for i in range (len(arr)):
-    pro=arr[i]
-    for j in range (i+1,n):
-        pro*=arr[j]
-        ans=max(ans,pro)
-print(ans)
+
+#brute force
+
+# arr=[1,2,-3,0,-4,-5]
+# ans=float('-inf')
+# for i in range (len(arr)):
+#     pro=arr[i]
+#     for j in range (i+1,len(arr)):
+#         pro*=arr[j]
+#         ans=max(ans,pro)
+# print(ans)
         
