1df797593fb962be8833188fcc116d7226b6bdb6
Yogeshcff <yugweeb@gmail.com>
2026-06-22 12:15:55 +0530

    solution

---
+++ Binary search/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+arr=[1,2,3,3,7,8,9,9,9,11]
+# x=3
+# ans=10
+# low=0
+# high=len(arr)-1
+# while low <=high:
+#     mid=low+(high-low)//2
+#     if arr[mid]>=x:
+#         ans=mid
+#         high=mid-1
+#     elif arr[mid]<x:
+#         low=mid+1
+# print(ans)
+    
