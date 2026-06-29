0f3fa5007f638e27792f63fa765ff4d4f65ab661
Yogeshcff <yugweeb@gmail.com>
2026-06-29 14:11:31 +0530

    solution

---
+++ Binary search/binary search on 1d array/basics.py
@@ -0,0 +1,30 @@
+# def binarysearch(arr,target):
+    
+#     n=len(arr)
+#     target=13
+#     low=0
+#     high=n-1
+#     while low<=high:
+#         mid=(low+high)//2
+#         if arr[mid]>target:
+#             high=mid-1
+#         elif arr[mid]<target:
+#             low=mid+1
+#         else:
+#             return mid
+#     return -1
+# print(binarysearch([2,4,6,7,9,11,18,19],13))
+
+def bs(nums,low,high,target):
+    if low>high:
+        return -1
+    mid=low+(high-low)//2
+    if nums[mid]==target:
+        return mid
+    elif nums[mid]<target :
+        return bs(nums,mid+1,high,target)
+    else:
+        return bs(nums,low,mid-1,target)
+nums= [2,4,6,7,9,11,18,119]
+n=len(nums)-1  
+print(bs(nums,0,n,0))
